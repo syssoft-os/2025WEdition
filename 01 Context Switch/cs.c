fb8f69e489c9b823b5a1b1dd5bdbbf5ecbf35e72
Peter Sturm <sturm@uni-trier.de>
2025-10-28 11:45:11 +0100

    Measure context switch latencies

---
+++ 01 Context Switch/cs.c
@@ -0,0 +1,25 @@
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main ( int argc, char *argv[] ) {
+
+    // Number of iterations
+    int iterations = 1000000;
+
+    // Measure time
+    clock_t start = clock();
+
+    // System call loop
+    for (int i = 0; i < iterations; i++) {
+        sleep(0);
+    }
+
+    // Messure time
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time taken: %f seconds\n", time_spent);
+    printf("Iterations: %d\n", iterations);
+    printf("Average time per call: %f seconds\n", time_spent / iterations);
+    return 0;
+}
